44dee3469cf892e46e3d75062125779392b0ae65
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-27 18:11:32 +0300

    Global.h added which contains Auto Generated Files Names globally

---
+++ hospital-management-oop-cpp/Global.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+// File paths
+const string PATIENTS_FILE = "Data/patients.txt";
+const string DOCTORS_FILE = "Data/doctors.txt";
+const string APPOINTMENTS_FILE = "Data/appointments.txt";
